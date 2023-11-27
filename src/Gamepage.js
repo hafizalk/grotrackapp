1b65f9e1af672181cb701e18fac8afe811e907c8
unknown <alakobanks@gmail.com>
2023-11-27 00:47:38 +0000

    ticktick1: listen for game results and display

--- src/Gamepage.js
+++ src/Gamepage.js
@@ -6,22 +6,34 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import useModal, { socket } from "./index";
-import { Button, FormGroup, Input, Table } from "reactstrap";
+import { Button, Col, FormGroup, Input, Table } from "reactstrap";
 import React, { useEffect, useReducer, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
 import Banner from "Banner";
 
-const Gamepage = () => {
+const Gamepage = ({ server }) => {
   const navigate = useNavigate();
   const [roomId, setRoomId] = useState(localStorage.getItem("roomId"));
   const [doesGameStop, setGameStop] = useState(false);
+  const [animalName, setAnimalName] = useState(null);
+  const [countryName, setCountryName] = useState(null);
+  const [objectName, setObjectName] = useState(null);
+  const [result, setResult] = useState(null);
+  const [selectedLetter, setSelectedletter] = useState(
+    localStorage.getItem("selectedLetter")
+  );
 
   useEffect(() => {
     function onGameStopEvent(value) {
       console.log(value);
-      setGameStop(value.stopGame);
+      socket.emit("collateResults", {
+        roomId,
+        email: localStorage.getItem("email"),
+        gameEntries: { animalName, countryName, objectName },
+        selectedLetter: localStorage.getItem("selectedLetter"),
+      });
     }
 
     socket.on("stopGame", onGameStopEvent);
@@ -29,35 +41,74 @@ const Gamepage = () => {
     return () => {
       socket.off("stopGame", onGameStopEvent);
     };
-  }, [roomId]);
+  }, [animalName, countryName, objectName, localStorage]);
 
   useEffect(() => {
-    if (doesGameStop) {
-      navigate("/");
+    socket.connect();
+    socket.emit("joinGame", {
+      roomId,
+      email: localStorage.getItem("email"),
+    });
+    function onGameResultEvent(value) {
+      console.log(value);
+      setResult(value.result);
     }
-  }, [doesGameStop]);
+
+    socket.on(roomId.concat("result"), onGameResultEvent);
+
+    return () => {
+      socket.off(roomId.concat("result"), onGameResultEvent);
+    };
+  }, []);
+
+  const DisplayData = ({ result }) =>
+    result.map((res) => {
+      return (
+        <tr key={res.key}>
+          <td>{res.email}</td>
+          <td>{res.score}</td>
+        </tr>
+      );
+    });
 
   return (
     <div>
       <Banner />
+      <div className="d-flex p-5 justify-content-around">
+        <Col md={5}>
+          <span>Selected Letter: </span>
+        </Col>
+        <Col md={5}>
+          <Input value={selectedLetter} disabled />
+        </Col>
+      </div>
       <Table>
         <thead>
           <tr>
-            <th>Animal</th>
-            <th>Place</th>
-            <th>Thing</th>
+            <th>Animal Name</th>
+            <th>Country Capital</th>
+            <th>Object Name</th>
           </tr>
         </thead>
         <tbody>
           <tr className="table-primary">
             <td>
-              <Input />
+              <Input
+                value={animalName}
+                onChange={(e) => setAnimalName(e.target.value)}
+              />
             </td>
             <td>
-              <Input />
+              <Input
+                value={countryName}
+                onChange={(e) => setCountryName(e.target.value)}
+              />
             </td>
             <td>
-              <Input />
+              <Input
+                value={objectName}
+                onChange={(e) => setObjectName(e.target.value)}
+              />
             </td>
           </tr>
         </tbody>
@@ -68,10 +119,24 @@ const Gamepage = () => {
         onClick={(e) => {
           e.preventDefault();
           socket.emit("stopGame", { roomId });
+          setGameStop(true);
         }}
       >
         Stop Game
       </Button>
+      {result && (
+        <div>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Player</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody>{DisplayData({ result })}</tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
--- src/Ticktick.js
+++ src/Ticktick.js
@@ -1,8 +1,12 @@
-import { faSignOut, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faCopy, faSignOut, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   Button,
+  Col,
   Collapse,
+  Form,
+  FormGroup,
+  Input,
   Nav,
   Navbar,
   NavbarToggler,
@@ -15,14 +19,19 @@ import Select from "react-select";
 import { io } from "socket.io-client";
 import { serverUrl, socket } from "index";
 import Banner from "Banner";
+import { set } from "lodash";
+import { useCopyToClipboard } from "usehooks-ts";
+import { toast } from "react-toastify";
 
-const Ticktick = ({server}) => {
+const Ticktick = ({ server }) => {
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedLetter, setSelectedletter] = useState("A");
+  const [selectedLetter, setSelectedletter] = useState(null);
   const [roomId, setRoomId] = useState(null);
-
+  const [roomCreated, setRoomCreated] = useState(false);
+  const [value, copy] = useCopyToClipboard();
   const [isConnected, setIsConnected] = useState(socket.connected);
+  const [joinedRoom, setJoinedRoom] = useState(false);
 
   useEffect(() => {
     socket.connect();
@@ -42,21 +51,24 @@ const Ticktick = ({server}) => {
     }
 
     function onJoinedRoomEvent(value) {
-      console.log("room joined");
+      if (value.status == "playerExists") {
+        toast.error("Player already joined game");
+      } else if (value.status == "playerJoined") {
+        toast.success("Player successfully joined game");
+      }
+      navigate("/game");
     }
 
     socket.on("connect", onConnect);
     socket.on("disconnect", onDisconnect);
     socket.on("roomCreated", onGameStartEvent);
-    if (roomId) {
-      socket.on(roomId, onJoinedRoomEvent);
-    }
+    socket.on(roomId, onJoinedRoomEvent);
 
     return () => {
       socket.off("connect", onConnect);
       socket.off("disconnect", onDisconnect);
       socket.off("roomCreated", onGameStartEvent);
-      socket.off("roomId", onJoinedRoomEvent);
+      socket.off(roomId, onJoinedRoomEvent);
     };
   }, [roomId]);
 
@@ -76,43 +88,83 @@ const Ticktick = ({server}) => {
 
   return (
     <div>
-      <Banner/>
-      <div className="d-flex p-5 justify-content-center">
-        <Select
-          options={options}
-          value={selectedLetter}
-          onChange={(e) => {
-            setSelectedletter(e);
-          }}
-          placeholder="Choose letter for Tick tick"
-        />
-      </div>
+      <Banner />
+
       <div className="d-flex p-5 justify-content-around">
-        {isConnected && (
-          <Button
-            color="primary"
-            outline
-            onClick={(e) => {
-              e.preventDefault();
-              socket.emit("createRoom", { selectedLetter });
+        <Col md={6}>
+          <Select
+            options={options}
+            value={selectedLetter}
+            onChange={(e) => {
+              setSelectedletter(e);
             }}
-          >
-            Start Game
-          </Button>
-        )}
-        {roomId && (
+            placeholder="Choose letter for Tick tick"
+          />
+        </Col>
+        <Col md={6}>
+          {isConnected && selectedLetter && (
+            <Button
+              color="primary"
+              outline
+              onClick={(e) => {
+                e.preventDefault();
+                socket.emit("createRoom", { selectedLetter });
+                setRoomCreated(true);
+              }}
+            >
+              Start Game
+            </Button>
+          )}
+        </Col>
+      </div>
+      {roomCreated && (
+        <div className="d-flex p-5 justify-content-around">
+          <Col md={9}>
+            <Input
+              id="roomId"
+              placeholder="Room Id"
+              value={roomId}
+              disabled={true}
+              type="text"
+            />
+          </Col>
+          <Col md={3}>
+            <FontAwesomeIcon
+              id="copyIcon"
+              icon={faCopy}
+              title={"Copy Room Id"}
+              size="lg"
+              onClick={() => copy(roomId)}
+            />
+          </Col>
+        </div>
+      )}
+      <div className="d-flex p-5 justify-content-around">
+        <Col md={7}>
+          <Input
+            id="joinRoom"
+            placeholder="Enter Room Id to Join"
+            type="text"
+            onChange={(e) => setRoomId(e.target.value)}
+          />
+        </Col>
+        <Col md={5}>
           <Button
             color="success"
             onClick={(e) => {
               e.preventDefault();
               localStorage.setItem("roomId", roomId);
-              socket.emit("joinGame", { roomId });
-              navigate("/game");
+              localStorage.setItem("selectedLetter", selectedLetter.value);
+              socket.emit("joinGame", {
+                roomId,
+                email: localStorage.getItem("email"),
+              });
+              setJoinedRoom(true);
             }}
           >
             Join Game
           </Button>
-        )}
+        </Col>
       </div>
     </div>
   );
